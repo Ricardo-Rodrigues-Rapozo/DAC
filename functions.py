a2bc660e5be0dd63f3454e52c003bd308df1865e
Ricardo Rodrigues Rapozo <92274024+Ricardo-Rodrigues-Rapozo@users.noreply.github.com>
2024-11-13 20:05:07 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -14,25 +14,17 @@ def senoide(A,f,Fs,phi,num_samples):
     phi(float): angulo de atraso (rad)
     num_samples(int): numero de amostras do vetor 
     """
-
     t = np.arange(0, num_samples) / Fs
-    # Gera a senoide
-    y = A * np.sin(2 * np.pi * f * t + phi)
-     # Ajusta a senoide para não ter valores negativos
-    #y = y - y.min()
-    return y,t
+    y = A * np.sin(2 * np.pi * f * t + phi) + 1.75  # Ajusta para valores não negativos
+    return y, t
 
 
 #Função para enviar sinal contínuo para o microcontrolador
 def send_sync_signal(ser,buffer,Size_buffer):
-    sync_byte = bytes([1])  # Envia o byte 0xAA
     # Supondo que y_uint16 seja o seu vetor de valores
     for value in buffer:
-    # Converte `value` em um valor `uint16_t` em formato de 2 bytes (little-endian)
-        byte_data = struct.pack('<H', value)  # '<H' significa 'unsigned short' (16 bits)
-    # Agora você pode enviar `byte_data` pela UART ou outro canal de comunicação
-    #for i in range(Size_buffer):
-        ser.flush()
-        ser.write(byte_data)
+ 
+        ser.write(np.float32(value*100).tobytes())
     print("fim")
-    time.sleep(0.1)  # Espera 1 segundo antes de enviar o próximo sinal
+    #time.sleep(0.1)  # Espera 1 segundo antes de enviar o próximo sinal
+
