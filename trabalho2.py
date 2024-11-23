1ebec74dcdc940285ee38c9f8c9eeafc932a2e9e
Ricardo Rodrigues Rapozo <92274024+Ricardo-Rodrigues-Rapozo@users.noreply.github.com>
2024-11-22 23:00:55 -0300

    Update trabalho2.py

--- trabalho2.py
+++ trabalho2.py
@@ -7,12 +7,10 @@ import functions
 # Parâmetros da senoide
 A = 1        # Amplitude
 f = 800        # Frequência (Hz)
-Fs = 40000    # Frequência de amostragem (Hz)
+Fs = 80000    # Frequência de amostragem (Hz)
 phi = 0      # Fase (radianos)
-BUFFER_SIZE = 100
+BUFFER_SIZE = 99
 #FS = 10000
-valor_inteiro = 1
-data = []
 n = Fs/f ## n amostras por ciclo 
 print(n)
 
@@ -20,13 +18,14 @@ vect = np.zeros(BUFFER_SIZE, dtype=np.uint16)
 # Gera a senoide
 y,t = functions.senoide(A, f, Fs, phi,BUFFER_SIZE)
  # Normalizando o valor de y para o intervalo [0, 65535]
-    
-plt.plot(t,y)
-plt.show()
+
+#plt.plot(t,y)
+#plt.show()
 #Configura a porta serial  
 ser = serial.Serial('COM3', 115200, timeout=None)
-ser.flush() ## limpa dados pendentes na porta serial 
+ser.reset_input_buffer() ## limpa dados pendentes na porta serial 
 time.sleep(1)  # Espera a conexão ser estabelecida
 
 while True:     
+    #ser.reset_input_buffer()
     functions.send_sync_signal(ser,y,BUFFER_SIZE)
